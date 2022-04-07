4dd0a1cd645d1a274dc1f9c4fb0938e591c43f34
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2022-04-07 13:58:50 +0800

    update: domains folder & optimizer.py

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# 「小 X 知识百科网」清单
+# 「小搭百科网」类内容农场网站清单
 
-Google 中文搜索结果包含了相当一部分的内容农场式条目，其中多为「小 X 知识网」「小 X 百科网」。访问此种链接会 302 重定向至 g.penzai.com，页面内容为自动生成，大量堆叠关键字，揉杂一些爬取到的内容，完全不具可读性和参考价值。
+Google 中文搜索结果包含了相当一部分的内容农场式条目，比如「小 X 知识网」「小 X 百科网」。此种链接常会 302 重定向其主站，页面内容为自动生成，大量堆叠关键字，揉杂一些爬取到的内容，完全不具可读性和参考价值。
 
-尤为过分的是，该网站有成千上万个域名被 Google 收录，严重影响搜索体验。详见社区反馈：
+尤为过分的是，该类网站可能有成千上万个分身域名被 Google 收录，严重影响搜索体验。详见 2021 年 10 初的社区反馈：
 
 1. [Github: 如何屏蔽“小搭百科网”？](https://github.com/cobaltdisco/Google-Chinese-Results-Blocklist/issues/50)
 2. [V2EX: 请问在 google 搜索时，频繁遇到小 X 知识网等内容农场式结果，怎么办？](https://www.v2ex.com/t/806025)
@@ -10,7 +10,9 @@ Google 中文搜索结果包含了相当一部分的内容农场式条目，其�
 4. [HOSTLOC: 这采集站群太强了吧](https://hostloc.com/thread-902528-1-1.html)
 5. [HOSTLOC: 小*知识网站群是哪位大佬的杰作](https://hostloc.com/thread-902496-1-1.html)
 
-该站群中的站点并不都在标题中包含「百科网」「知识网」的字样，所以为方便广大网友过滤搜索结果，特整理此清单。
+使用正则匹配标题的方式不能完全屏蔽，所以为方便广大网友过滤搜索结果，特整理此清单。
+
+由于此次事件主角「小搭百科网」在造成影响后[主动关站](https://www.v2ex.com/t/807150 )，所以接下来也将关注、收录其他的类似内容农场站。（所以 penzai-list 可以解释成「翉粪水喷涌之灾清单」？）
 
 ## 使用方式
 
@@ -35,6 +37,17 @@ Google 中文搜索结果包含了相当一部分的内容农场式条目，其�
 
 1. 从本地插件 uBlacklist 中导出域名列表
 2. 在搜索引擎中尝试长尾关键词，以发现更多目前权重尚低的农场域名
+
+| 文件 | 说明 |  
+| -- | -- |  
+| `uBlacklist.txt` | uBlacklist 规则集合 |  
+| `Surge.txt` | Surge 规则集合 |
+| `uBlacklist/spam/g.penzai.com.txt` | uBlacklist 专用小搭百科网域名集合|  
+| `Surge/spam/g.penzai.com.txt` | Surge 专用小搭百科网域名集合|
+| `uBlacklist/machine-translated/stackoverflow.txt` | uBlacklist 专用机翻 StackOverflow 域名集合|  
+| `Surge/machine-translated/stackoverflow.txt` | Surge 专用机翻 StackOverflow 域名集合|
+
+按结构在 `domains` 目录中添加新的分类集合文件。参考文件中已有内容的格式，在任意位置添加即可。（Fork 本仓库后编辑再 Push，或在页面中编辑均可。）
 
 ### 二、举报
 
--- scripts/optimizer.py
+++ scripts/optimizer.py
@@ -9,7 +9,24 @@ log = logging.getLogger(__name__)
 
 
 def main():
-    domains = read_all()
+    optimize('domains', '')
+    pass
+
+
+def optimize(input_path, output_root):
+    domains = set()
+    if os.path.isfile(input_path) and input_path.endswith(".txt"):
+        domains.update(read_file(input_path, r'^\*://(?:\*\.)?([\w\-\.]+)/\*'))
+        output_root = os.path.join(output_root, os.path.splitext(os.path.basename(input_path))[0])
+    else:
+        files = os.listdir(input_path)
+        for file in files:
+            path = os.path.join(input_path, file)
+            file_output_root = os.path.join(output_root, file) if os.path.isdir(path) else output_root
+            domains.update(optimize(path, file_output_root))
+
+    log.info('Read %s unique lines for %s' % (len(domains), input_path))
+
     domain_set = set()
     domain_list = []
     for domain in domains:
@@ -20,42 +37,40 @@ def main():
 
     domain_list.extend(domain_set)
     domain_list.sort()
-    write_all(domain_list)
+    write_all(domain_list, output_root)
+
+    return domain_list
 
 
-def read_all():
-    domains = set()
-    domains.update(read_ublacklist())
-    domains.update(read_surge())
-    log.info('Read %s  unique lines' % len(domains))
-    return domains
-
-
-def write_all(domains):
-    write_ublacklist(domains)
-    write_surge(domains)
+def write_all(domains, output_root):
+    write_ublacklist(domains, output_root)
+    write_surge(domains, output_root)
     pass
 
 
-def read_file(fname, pattern):
+def read_file(file_path, pattern):
     domains = []
-    if os.path.exists('../' + fname):
-        with open('../' + fname) as f:
+    if os.path.exists(file_path):
+        with open(file_path) as f:
             for line in f.readlines():
                 m = re.match(pattern, line)
                 if m and len(m.groups()) > 0:
                     domains.append(m.group(1))
                 pass
             pass
-        log.info('Read %s lines from %s' % (len(domains), fname))
+        log.info('Read %s lines from %s' % (len(domains), file_path))
     else:
-        log.warning('No such file or directory: %s' % '../' + fname)
+        log.warning('No such file or directory: %s' % file_path)
     return domains
 
 
-def write_file(fname, domains, wrapper):
+def write_file(file_path, domains, wrapper):
     lines = []
-    with open('../' + fname, 'w') as f:
+    parent_path = os.path.dirname(file_path)
+    if parent_path != '' and not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(parent_path)
+
+    with open(file_path, 'w') as f:
         for domain in domains:
             lines.append(wrapper(domain))
 
@@ -68,8 +83,8 @@ def read_ublacklist():
     return read_file('uBlacklist.txt', r'^\*://(?:\*\.)?([\w\-\.]+)/\*')
 
 
-def write_ublacklist(domains):
-    write_file('uBlacklist.txt', domains, lambda x: '*://*.%s/*\n' % x if is_domain(x) else '*://%s/*\n' % x)
+def write_ublacklist(domains, output_root):
+    write_file(os.path.join('uBlacklist', output_root + '.txt') if output_root != '' else 'uBlacklist.txt', domains, lambda x: '*://*.%s/*\n' % x if is_domain(x) else '*://%s/*\n' % x)
     pass
 
 
@@ -77,8 +92,8 @@ def read_surge():
     return read_file('Surge.txt', r'^(?:\.)?([\w\-\.]+)')
 
 
-def write_surge(domains):
-    write_file('Surge.txt', domains, lambda x: '.%s\n' % x if is_domain(x) else '#%s\n' % x)
+def write_surge(domains, output_root):
+    write_file(os.path.join('Surge', output_root + '.txt') if output_root != '' else 'Surge.txt', domains, lambda x: '.%s\n' % x if is_domain(x) else '#%s\n' % x)
     pass
 
 
